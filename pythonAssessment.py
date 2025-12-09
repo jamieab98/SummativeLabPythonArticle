2013947a41c98677746892d30ab88403e7fa865b
Jamie Bryson <jamieab98@gmail.com>
2025-12-09 18:36:02 -0500

    Noticed that the article used curly quotes intead of normal quotes and that's what was causing the bug

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -3,13 +3,13 @@ import string
 with open("news_article.txt", "r") as f:
     article = f.read()
 
-sample = "The dog barked. The cat hisses."
+sample = 'Peter apple snapple apple.'
 
 def count_specific_word(text, word):
     words = text.split()
     count = 0
     for w in words:
-        w_clean = w.lower().strip(string.punctuation + string.whitespace)
+        w_clean = w.lower().strip(string.punctuation + string.whitespace).strip('“')
         if w_clean == word.lower().strip(string.punctuation + string.whitespace):
             count += 1
     return count
