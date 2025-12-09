4c8ebc4a9699e6fbcb7256eb8584061d99128e35
Jamie Bryson <jamieab98@gmail.com>
2025-12-09 18:19:26 -0500

    git the first one working but there is a slight bug where 'apple' ony returns 15 instead of 16

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -1,4 +1,18 @@
+import string
+
 with open("news_article.txt", "r") as f:
     article = f.read()
 
-print(article)
+sample = "The dog barked. The cat hisses."
+
+def count_specific_word(text, word):
+    words = text.split()
+    count = 0
+    for w in words:
+        w_clean = w.lower().strip(string.punctuation + string.whitespace)
+        if w_clean == word.lower().strip(string.punctuation + string.whitespace):
+            count += 1
+    return count
+
+result = count_specific_word(article, 'apple')
+print(result)
