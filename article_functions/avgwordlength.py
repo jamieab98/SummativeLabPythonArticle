610b1dbdc562b6fac945c8fa0ef85d627d9aac23
Jamie Bryson <jamieab98@gmail.com>
2025-12-10 16:52:12 -0500

    Cleaned up the code

--- article_functions/avgwordlength.py
+++ article_functions/avgwordlength.py
@@ -1,6 +1,5 @@
 import string
 def calculate_average_word_length(text):
-    #iterate through every word in the text and turn them into their pure string form
     words = text.split()
     clean_words = []
     for w in words:
@@ -25,8 +24,3 @@ def calculate_average_word_length(text):
     else:
         average = total_length / len(length_list)
     print(average)
-    #make a a list of every unique word (repeated words don't matter)
-
-    #iterate through every word in the unique word list and make an new list consisting of the length of each unique word
-
-    #add up all the values (maybe by iterating through the list) and then divide by the number of unique words
