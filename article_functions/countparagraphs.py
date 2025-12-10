d75e54e6af93626e7a083ee7b6b5f3a63fcca216
Jamie Bryson <jamieab98@gmail.com>
2025-12-10 18:06:14 -0500

    completed the paragraph counter

--- article_functions/countparagraphs.py
+++ article_functions/countparagraphs.py
@@ -1,3 +1,14 @@
 def count_paragraphs(text):
-    lines = text.splitlines()
-    print(lines)
+    paragraphs = text.splitlines()
+    paragraph_count = 1
+    ticker = 0
+
+    for paragraph in paragraphs:
+        if paragraph == "":
+            ticker += 1
+        elif paragraph != "":
+            ticker = 0
+        if ticker == 2:
+            ticker = 0
+            paragraph_count += 1
+    print(paragraph_count)
