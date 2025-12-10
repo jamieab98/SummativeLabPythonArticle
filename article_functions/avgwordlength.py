ea28a6b54dee23949cec6de163ffb7b8fdb7cbd3
Jamie Bryson <jamieab98@gmail.com>
2025-12-10 16:24:20 -0500

    iterated through the words and made them free of punctuation and lowercase. Then stored each unique word in the clean_words list

--- article_functions/avgwordlength.py
+++ article_functions/avgwordlength.py
@@ -2,7 +2,14 @@ import string
 def calculate_average_word_length(text):
     #iterate through every word in the text and turn them into their pure string form
     words = text.split()
-    print(words)
+    clean_words = []
+    for w in words:
+        clean_word = w.strip(string.punctuation + string.whitespace + '“”').lower()
+        if clean_word not in clean_words:
+            clean_words.append(clean_word)
+            print(clean_word)
+    print(clean_words)
+
     #make a a list of every unique word (repeated words don't matter)
 
     #iterate through every word in the unique word list and make an new list consisting of the length of each unique word
