91892b4795f1e0111fe1e34fb05d986ee3aaa3ab
Jamie Bryson <jamieab98@gmail.com>
2025-12-10 16:40:23 -0500

    Made the code ignore \ufeff when ceraning the clean_words list

--- article_functions/avgwordlength.py
+++ article_functions/avgwordlength.py
@@ -5,12 +5,17 @@ def calculate_average_word_length(text):
     clean_words = []
     for w in words:
         clean_word = w.strip(string.punctuation + string.whitespace + '“”').lower()
-        if clean_word not in clean_words:
+        if clean_word == "\ufeff":
+            exit
+        elif clean_word == "":
+            exit
+        elif clean_word not in clean_words:
             clean_words.append(clean_word)
     
     length_list = []
     for word in clean_words:
         length_list.append(len(word))
+    print (length_list)
 
 
     #make a a list of every unique word (repeated words don't matter)
