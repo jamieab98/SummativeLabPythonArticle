325cbff54ef57adf23f25e5a8fb4d65fa5c95eab
Jamie Bryson <jamieab98@gmail.com>
2025-12-11 17:29:08 -0500

    Made the count sentences funciton based off of the assignment description.

--- article_functions/countsentences.py
+++ article_functions/countsentences.py
@@ -1,2 +1,13 @@
 def count_sentences(text):
-    print("hello")
+    sentence_separator = (".", "!", "?")
+    sentences = text.split()
+
+    if text == "":
+        sentence_count = 1
+    else:
+        sentence_count = 0
+
+    for sentence in sentences:
+        if sentence[-1] in sentence_separator:
+            sentence_count += 1
+    print(sentence_count)
